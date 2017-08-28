e28b418253c73bc0a0fe99f7b9c619116b0ed6e5
edwinaquino <github@edwinaquino>
2017-08-28 15:02:54 -0700

    add connection to db

--- log-analysis.py
+++ log-analysis.py
@@ -6,5 +6,16 @@ import psycopg2
 def main():
     print('Log Analysis Script started...');
 
+    # Connection to database
+    conn = psycopg2.connect("dbname=news")
+
+    # Cursor for database connection
+    cur = conn.cursor()
+
+    # Question 1 - What are the most popular three articles of all time?
+
+
+
+
 if __name__ == "__main__":
  main()
